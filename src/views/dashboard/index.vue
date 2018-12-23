ced3e74e982f634d188091285faaf8e659aab131
wnagtao <1259709654@qq.com>
2018-12-23 23:35:54 +0800

    【20181223】通过vuex的role增加页面的权限控制

--- src/views/dashboard/index.vue
+++ src/views/dashboard/index.vue
@@ -2,6 +2,20 @@
   <div class="dashboard-container">
     <div class="dashboard-text">name:{{name}}</div>
     <div class="dashboard-text">role:<span v-for='role in role' :key='role'>{{role}}</span></div>
+    <h4>按钮权限限制</h4>
+    <div v-if="role === 'admin'">
+      <el-button>admin</el-button>
+      <el-button type="primary">admin</el-button>
+
+    </div>
+    <div v-if="role === 'editor3'">
+      <el-button type="success">editor3</el-button>
+      <el-button type="danger">editor3</el-button>
+    </div>
+    <div v-if="role === 'boss'">
+      <el-button type="success">boss</el-button>
+      <el-button type="danger">boss</el-button>
+    </div>
   </div>
 </template>
 
@@ -15,6 +29,9 @@ export default {
       'name',
       'role'
     ])
+  },
+  created () {
+
   }
 }
 </script>
