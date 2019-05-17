a0f76d04cf1c4394748e273112c1e08f4c09156e
wangtao02 <wangt02@zendaimoney.com>
2019-05-17 10:35:49 +0800

    修改配置文件的静态文件地址变量

--- vue.config.js
+++ vue.config.js
@@ -12,7 +12,7 @@ module.exports = {
     // 指定子路径。比如，如果你的应用部署在
     // https://www.foobar.com/my-app/
     // 那么将这个值改为 `/my-app/`
-    baseUrl: process.env.VUE_APP_PUBLIC_URL,
+    publicPath: process.env.VUE_APP_PUBLIC_URL,
 
     // 将构建好的文件输出到哪里
     outputDir: 'dist',
